3c282097824c0a49f47a42a7e025919a03ce59a6
Antonio de Castro Alves <antoniodecastroalves@gmail.com>
2020-05-04 10:30:50 -0300

    Import files TS

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => response.json({ message: 'Hello Gostack' }));
+app.use(routes);
 
 app.listen(3333, () => {
-  console.log('<< Server started on port 3333!>>');
+  console.log('< Server started on port 3333! >');
 });
