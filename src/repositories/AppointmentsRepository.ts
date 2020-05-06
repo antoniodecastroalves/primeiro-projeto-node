ef7792186318662250b79d674175d5f47ae4c94d
Antonio de Castro Alves <antoniodecastroalves@gmail.com>
2020-05-06 19:19:44 -0300

    Create Repositories

---
+++ src/repositories/AppointmentsRepository.ts
@@ -0,0 +1,28 @@
+import { isEqual } from 'date-fns';
+import Appointment from '../models/Appointment';
+
+class AppointmentRespository {
+  private appoinments: Appointment[];
+
+  constructor() {
+    this.appoinments = [];
+  }
+
+  public findByDate(date: Date): Appointment | null {
+    const findAppointment = this.appoinments.find(appointment =>
+      isEqual(date, appointment.date),
+    );
+
+    return findAppointment || null;
+  }
+
+  public create(provider: string, date: Date): Appointment {
+    const appointment = new Appointment(provider, date);
+
+    this.appoinments.push(appointment);
+
+    return appointment;
+  }
+}
+
+export default AppointmentRespository;
